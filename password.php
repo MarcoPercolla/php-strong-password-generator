e58e00974862b04efcd66745a0c1b87555d367d0
ReDMallard <marco@titori.it>
2023-12-20 03:27:57 +0100

    minor

--- password.php
+++ password.php
@@ -4,11 +4,11 @@
  function createPassword(){
     $passwordSet = $_GET["passwordSet"];
     $password = array();
-    $pool = '!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+    $pool = '!$&0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^-_abcdefghijklmnopqrstuvwxyz~';
     $poolLast = strlen($pool) - 1;
     for ($i=0; $i < $passwordSet ; $i++) { 
         $randomInt = rand(1,$poolLast);
-        $password[]=$pool[$randomInt];
+        array_push($password,$pool[$randomInt]);
 
         
     }
